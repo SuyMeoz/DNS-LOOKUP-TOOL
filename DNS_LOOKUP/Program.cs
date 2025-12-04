34a38b15e1f3cbe0e7ba51269ba51ab0f1e300d1
ASO <hung2k05@outlook.com>
2025-12-04 16:56:51 +0700

    DNS_LOOKUP version 1.0
    Công cụ được thiết kế và lập trình bởi Mướp The Lỏ, nếu thấy công cụ có vẻ lỏ vui lòng xem lại tên người phát triển

---
+++ DNS_LOOKUP/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DnsLookupTool
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainForm());
+        }
+    }
+}
